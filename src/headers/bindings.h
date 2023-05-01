ade40f77280411aadb6b5cd70ac149303cc3d7ab
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-05-01 12:20:03 +0200

    created build script + rust bindings

--- src/headers/bindings.h
+++ src/headers/bindings.h
@@ -1,6 +1,6 @@
 //This file is the entry-point for c-bindgen and also defines which symbols
 //should be provided by a geometry.
 
-#include "metric.h"
+#include "spacetimemetric.h"
 #include "christoffel.h"
 #include "riemann.h"
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod bindings;
+
 fn main() {
   
 }
