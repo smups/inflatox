9bcbbfde0bc66802f42e59c5e0848d592d9ca479
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-05-01 11:56:48 +0200

    spacetime bindings

---
+++ src/headers/bindings.h
@@ -0,0 +1,6 @@
+//This file is the entry-point for c-bindgen and also defines which symbols
+//should be provided by a geometry.
+
+#include "metric.h"
+#include "christoffel.h"
+#include "riemann.h"
