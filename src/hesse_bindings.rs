666f1fa0a924ee48ebd5d455696eba37084d93fb
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-05-31 17:01:15 +0200

    wrote rust bindings for the c shared lib

--- src/hesse_bindings.rs
+++ src/hesse_bindings.rs
@@ -1,15 +1,103 @@
-use std::path::Path;
+use std::ffi::OsStr;
 
+type HdylibFn<'a> = libloading::Symbol::<'a, unsafe extern fn (*const f64, *const f64) -> f64>;
+type HdylibStatic<'a> = libloading::Symbol::<'a, *const u32>;
 
-type HdylibFn = libloading::Symbol<unsafe extern fn (*const f64, *const f64) -> f64>;
-type HdylibStatic = libloading::Symbol<u32>;
+const SYM_DIM: &[u8; 3] = b"DIM";
+const PARAM_DIM: &[u8; 11] = b"N_PARAMTERS";
 
-struct HesseDylib {
-  lib: libloading::Library
+pub struct HesseDylib {
+  lib: libloading::Library,
+  f_dim: u32,
+  n_param: u32
 }
 
 impl HesseDylib {
-  pub fn new(path: &Path) -> Result<Self, std::io::Error> {
-    HesseDyLib { lib: unsafe { libloading::Library::new(path)? } }
+  pub fn new<P: AsRef<OsStr>>(path: P) -> Result<Self, libloading::Error> {
+    let lib = unsafe {
+      libloading::Library::new(path)?
+    };
+    let hesse_dim: u32 = unsafe {
+      let symbol: HdylibStatic = lib.get(SYM_DIM)?;
+      **symbol
+    };
+    let n_param: u32 = unsafe {
+      let symbol: HdylibStatic = lib.get(PARAM_DIM)?;
+      **symbol
+    };
+    Ok( HesseDylib { lib, f_dim: hesse_dim, n_param })
+  }
+
+  #[inline]
+  pub unsafe fn get_symbol<T>(&self, symbol: &[u8]) -> Result<libloading::Symbol<T>, libloading::Error> {
+    self.lib.get(symbol)
+  }
+
+  #[inline]
+  pub const fn get_dim(&self) -> usize {
+    self.f_dim as usize
+  }
+
+  #[inline]
+  pub const fn get_n_params(&self) -> usize {
+    self.n_param as usize
+  }
+}
+
+pub struct Hesse2D<'a> {
+  lib: &'a HesseDylib,
+  fns: [HdylibFn<'a>; 4]
+}
+
+impl<'a> Hesse2D<'a> {
+  pub fn new(lib: &'a HesseDylib) -> Result<Self, libloading::Error> {
+    unsafe {
+      let v00: HdylibFn = lib.get_symbol(b"v00")?;
+      let v01: HdylibFn = lib.get_symbol(b"v01")?;
+      let v10: HdylibFn = lib.get_symbol(b"v10")?;
+      let v11: HdylibFn = lib.get_symbol(b"v11")?;
+
+      Ok(Hesse2D { lib, fns: [v00, v01, v10, v11] })
+    }
+  }
+
+  #[inline]
+  pub fn v00(&self, x: &[f64], p: &[f64]) -> f64 {
+    assert!(x.len() == self.lib.get_dim());
+    assert!(p.len() == self.lib.get_n_params());
+    unsafe { self.fns[0](
+      x as *const [f64] as *const f64,
+      p as *const [f64] as *const f64
+    )}
+  }
+
+  #[inline]
+  pub fn v01(&self, x: &[f64], p: &[f64]) -> f64 {
+    assert!(x.len() == self.lib.get_dim());
+    assert!(p.len() == self.lib.get_n_params());
+    unsafe { self.fns[1](
+      x as *const [f64] as *const f64,
+      p as *const [f64] as *const f64
+    )}
+  }
+
+  #[inline]
+  pub fn v10(&self, x: &[f64], p: &[f64]) -> f64 {
+    assert!(x.len() == self.lib.get_dim());
+    assert!(p.len() == self.lib.get_n_params());
+    unsafe { self.fns[2](
+      x as *const [f64] as *const f64,
+      p as *const [f64] as *const f64
+    )}
+  }
+
+  #[inline]
+  pub fn v11(&self, x: &[f64], p: &[f64]) -> f64 {
+    assert!(x.len() == self.lib.get_dim());
+    assert!(p.len() == self.lib.get_n_params());
+    unsafe { self.fns[3](
+      x as *const [f64] as *const f64,
+      p as *const [f64] as *const f64
+    )}
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-pub mod hesse_bindings;
+mod hesse_bindings;
+use hesse_bindings::*;
 
 fn main() {
-  
+  let lib = HesseDylib::new("/tmp/libinflx_autoc_pz0lpkp_.so").unwrap();
+  println!("{}-{}", lib.get_dim(), lib.get_n_params());
+  let hesse_2d = Hesse2D::new(&lib).unwrap();
+  let v00 = hesse_2d.v00(&[1.0, 2.0], &[10.0, 20.0, 30.0]);
+  println!("v00(1,2) with p = [10, 20, 30] equals: {v00}")  
 }
