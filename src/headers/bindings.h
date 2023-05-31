e83d361bde85eea3de88da34c828693756445d22
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-05-31 16:06:59 +0200

    remove obsolete files

--- src/headers/bindings.h
+++
@@ -1,6 +0,0 @@
-//This file is the entry-point for c-bindgen and also defines which symbols
-//should be provided by a geometry.
-
-#include "spacetimemetric.h"
-#include "christoffel.h"
-#include "riemann.h"
