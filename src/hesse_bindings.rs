f43532f0d4ab0109084327c9fe26572d69ce6f6c
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-06-01 17:49:21 +0200

    quick and dirty anguelova impl

--- src/hesse_bindings.rs
+++ src/hesse_bindings.rs
@@ -3,32 +3,28 @@ use std::{ffi::OsStr, mem::MaybeUninit};
 use ndarray as nd;
 
 type HdylibFn<'a> = libloading::Symbol::<'a, unsafe extern fn (*const f64, *const f64) -> f64>;
-type HdylibStatic<'a> = libloading::Symbol::<'a, *const u32>;
+type HdylibStaticInt<'a> = libloading::Symbol::<'a, *const u32>;
+type HdyLibStaticArr<'a> = libloading::Symbol<'a, *const [u16; 3]>;
 
 const SYM_DIM_SYM: &[u8; 3] = b"DIM";
 const PARAM_DIM_SYM: &[u8; 11] = b"N_PARAMTERS";
 const POTENTIAL_SYM: &[u8; 1] = b"V";
+const INFLATOX_VERSION_SYM: &[u8; 7] = b"VERSION";
 
-pub struct HesseDylib {
+pub struct InflatoxDylib {
   lib: libloading::Library,
   f_dim: u32,
-  n_param: u32
+  n_param: u32,
+  inflatox_version: [u16; 3]
 }
 
-impl HesseDylib {
+impl InflatoxDylib {
   pub fn new<P: AsRef<OsStr>>(path: P) -> Result<Self, libloading::Error> {
-    let lib = unsafe {
-      libloading::Library::new(path)?
-    };
-    let hesse_dim: u32 = unsafe {
-      let symbol: HdylibStatic = lib.get(SYM_DIM_SYM)?;
-      **symbol
-    };
-    let n_param: u32 = unsafe {
-      let symbol: HdylibStatic = lib.get(PARAM_DIM_SYM)?;
-      **symbol
-    };
-    Ok( HesseDylib { lib, f_dim: hesse_dim, n_param })
+    let lib = unsafe { libloading::Library::new(path)? };
+    let f_dim = unsafe { ***lib.get::<HdylibStaticInt>(SYM_DIM_SYM)? };
+    let n_param = unsafe { ***lib.get::<HdylibStaticInt>(PARAM_DIM_SYM)? };
+    let inflatox_version = unsafe { ***lib.get::<HdyLibStaticArr>(INFLATOX_VERSION_SYM)? };
+    Ok( InflatoxDylib { lib, f_dim, n_param, inflatox_version })
   }
 
   #[inline]
@@ -45,16 +41,21 @@ impl HesseDylib {
   pub const fn get_n_params(&self) -> usize {
     self.n_param as usize
   }
+
+  #[inline]
+  pub const fn get_inflatox_version(&self) -> &[u16; 3] {
+    &self.inflatox_version
+  }
 }
 
 pub struct HesseNd<'a> {
-  lib: &'a HesseDylib,
+  lib: &'a InflatoxDylib,
   potential: unsafe extern fn (*const f64, *const f64) -> f64,
   components: nd::Array2<HdylibFn<'a>>
 }
 
 impl<'a>HesseNd<'a> {
-  pub fn new(lib: &'a HesseDylib) -> Result<Self, libloading::Error> {
+  pub fn new(lib: &'a InflatoxDylib) -> Result<Self, libloading::Error> {
     let dim = nd::Dim([lib.get_dim(), lib.get_dim()]);
     let mut array: nd::Array2<MaybeUninit<HdylibFn>> = nd::Array2::uninit(dim);
     let potential = unsafe { **lib.get_symbol::<HdylibFn>(POTENTIAL_SYM).unwrap() };
@@ -72,7 +73,17 @@ impl<'a>HesseNd<'a> {
     Ok(HesseNd { lib, potential, components: unsafe { array.assume_init() } })
   }
 
-  pub fn calc_hesse(&self, x: &[f64], p: &[f64]) -> nd::Array2<f64> {
+  #[inline(always)]
+  pub fn potential(&self, x: &[f64], p: &[f64]) -> f64 {
+    assert!(x.len() == self.lib.get_dim());
+    assert!(p.len() == self.lib.get_n_params());
+    unsafe { (self.potential)(
+      x as *const [f64] as *const f64,
+      p as *const [f64] as *const f64
+    )}
+  }
+
+  pub fn hesse(&self, x: &[f64], p: &[f64]) -> nd::Array2<f64> {
     assert!(x.len() == self.lib.get_dim());
     assert!(p.len() == self.lib.get_n_params());
     self.components.mapv(|func| unsafe {
@@ -82,18 +93,18 @@ impl<'a>HesseNd<'a> {
 }
 
 pub struct Hesse2D<'a> {
-  lib: &'a HesseDylib,
+  lib: &'a InflatoxDylib,
   potential: unsafe extern fn (*const f64, *const f64) -> f64,
-  fns: [HdylibFn<'a>; 4]
+  fns: [unsafe extern fn (*const f64, *const f64) -> f64; 4]
 }
 
 impl<'a> Hesse2D<'a> {
-  pub fn new(lib: &'a HesseDylib) -> Result<Self, libloading::Error> {
+  pub fn new(lib: &'a InflatoxDylib) -> Result<Self, libloading::Error> {
     unsafe {
-      let v00: HdylibFn = lib.get_symbol(b"v00")?;
-      let v01: HdylibFn = lib.get_symbol(b"v01")?;
-      let v10: HdylibFn = lib.get_symbol(b"v10")?;
-      let v11: HdylibFn = lib.get_symbol(b"v11")?;
+      let v00 = **lib.get_symbol::<HdylibFn>(b"v00")?;
+      let v01 = **lib.get_symbol::<HdylibFn>(b"v01")?;
+      let v10 = **lib.get_symbol::<HdylibFn>(b"v10")?;
+      let v11 = **lib.get_symbol::<HdylibFn>(b"v11")?;
       let potential = **lib.get_symbol::<HdylibFn>(POTENTIAL_SYM).unwrap();
 
       Ok(Hesse2D { lib, potential, fns: [v00, v01, v10, v11] })
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,45 @@
 mod hesse_bindings;
 use hesse_bindings::*;
 
+use ndarray as nd;
+use rayon::prelude::*;
+
 fn main() {
-  let lib = HesseDylib::new("/tmp/libinflx_autoc_9u8fmfgo.so").unwrap();
+  let lib = InflatoxDylib::new("/tmp/libinflx_autoc_majrfb8z.so").unwrap();
   println!("{}-{}", lib.get_dim(), lib.get_n_params());
   let hesse_2d = Hesse2D::new(&lib).unwrap();
-  let v00 = hesse_2d.v00(&[1.0, 2.0], &[10.0, 20.0, 30.0]);
+  let p = &[10.0, 20.0, 30.0, 10.0];
+  let v00 = hesse_2d.v00(&[1.0, 2.0], &[10.0, 20.0, 30.0, 10.0]);
   println!("v00(1,2) with p = [10, 20, 30] equals: {v00}");
 
-  let args = &[10.0, 50.0, 1.0, 1.5, 3.0];
-  test_3d(&[100.0, 500.0, 10.0], args);
-  /*
-    V0 -> args[0]
-    m -> args[1]
-    φ0 -> args[2]
-    θ0 -> args[3]
-    ψ0 -> args[4] 
-  */
+  calc_anguelova(&hesse_2d, p);
 }
 
 fn test_3d(x: &[f64], p: &[f64]) {
-  let lib = HesseDylib::new("/tmp/libinflx_autoc_oz8kke7_.so").unwrap();
+  let lib = InflatoxDylib::new("/tmp/libinflx_autoc_oz8kke7_.so").unwrap();
   let hesse_nd = HesseNd::new(&lib).unwrap();
-  let h = hesse_nd.calc_hesse(x, p);
+  let h = hesse_nd.hesse(x, p);
   println!("{h:?}");
+}
+
+#[inline]
+fn anguelova(h: &Hesse2D, x: &[f64], p: &[f64]) -> f64 {
+  let lhs = 3.0 * h.potential(x, p) * (h.v00(x, p) / h.v01(x, p).powi(2)).powi(2);
+  let rhs = h.v11(x, p);
+  return lhs - rhs;
+}
+
+fn calc_anguelova(h: &Hesse2D, p: &[f64]) {
+  let size = 10_000;
+  let grid_spacing = 1E-2;
+  let offst = (size / 2) as f64 * -grid_spacing;
+
+  let mut field_space = nd::Array2::zeros([size, size]);  
+  field_space
+    .indexed_iter_mut()
+    .par_bridge()
+    .map(|(idx, val)| ([idx.0 as f64 * grid_spacing + offst, idx.1 as f64 * grid_spacing + offst], val))
+    .for_each(|(x, val)| *val = anguelova(h, &x, p));
+
+  println!("{field_space:?}");
 }
