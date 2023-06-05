2a4824fec1692d382dec1f027ffc103aca5bfef4
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-06-05 11:50:19 +0200

    updated copyright notices

--- python/inflatox/consistency_conditions.py
+++ python/inflatox/consistency_conditions.py
@@ -2,16 +2,16 @@
 #
 #  This file is part of Inflatox.
 #
-#  rustronomy is free software: you can redistribute it and/or modify it under
+#  Inflatox is free software: you can redistribute it and/or modify it under
 #  the terms of the European Union Public License version 1.2 or later, as
 #  published by the European Commission.
 #
-#  rustronomy is distributed in the hope that it will be useful, but WITHOUT ANY
+#  Inflatox is distributed in the hope that it will be useful, but WITHOUT ANY
 #  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 #  A PARTICULAR PURPOSE. See the European Union Public License for more details.
 #
 #  You should have received a copy of the EUPL in an/all official language(s) of
-#  the European Union along with rustronomy.  If not, see 
+#  the European Union along with Inflatox.  If not, see 
 #  <https://ec.europa.eu/info/european-union-public-licence_en/>.
 #
 #  (1) Resident of the Kingdom of the Netherlands; agreement between licensor and
