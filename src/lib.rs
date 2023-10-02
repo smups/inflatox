c1aba7f4f1fe6d07d9f2f7c6426106b3dbd70b07
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-10-02 14:30:00 +0200

    added new pixel-flagging function to python mod

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,7 @@ fn libinflx_rs(py: Python<'_>, pymod: &PyModule) -> PyResult<()> {
   pymod.add_function(wrap_pyfunction!(open_inflx_dylib, pymod)?)?;
   pymod.add_function(wrap_pyfunction!(anguelova::anguelova_py, pymod)?)?;
   pymod.add_function(wrap_pyfunction!(anguelova::delta_py, pymod)?)?;
+  pymod.add_function(wrap_pyfunction!(anguelova::flag_quantum_dif, pymod)?)?;
 
   //Register exceptions
   pymod.add("DimensionalityError", py.get_type::<ShapeError>())?;
