4c78b1b95c7b6c89c083105e405a44f790ec35bc
Raúl Wolters <56522208+smups@users.noreply.github.com>
2024-12-10 15:01:52 +0100

    change model to something that does not generate NaN's

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -23,14 +23,12 @@ import sympy as sp
 
 
 def test_doc_example():
-    sp.init_printing()
-
     # define model
-    φ, θ, L, m, φ0 = sp.symbols("φ θ L m φ0")
-    fields = [φ, θ]
+    r, θ, m = sp.symbols("r θ m")
+    fields = [r, θ]
 
-    V = (1 / 2 * m**2 * (φ - φ0) ** 2).nsimplify()
-    g = [[1, 0], [0, L**2 * sp.sinh(φ / L) ** 2]]
+    V = (1 / 2 * m**2 * (θ**2 -2/(3 * r**2))).nsimplify()
+    g = [[0.5, 0], [0, 0.5 * r**2]]
 
     # symbolic calculation
     calc = inflatox.SymbolicCalculation.new(fields, g, V)
@@ -38,20 +36,23 @@ def test_doc_example():
 
     # run the compiler
     out = inflatox.Compiler(hesse).compile()
+    out.print_sym_lookup_table()
 
     # evaluate the compiled potential and Hesse matrix
     from inflatox.consistency_conditions import GeneralisedAL
 
     anguelova = GeneralisedAL(out)
 
-    params = np.array([1.0, 1.0, 1.0])
+    params = np.array([1.0])
     x = np.array([2.0, -2.0])
-    assert anguelova.calc_V(x, params) == 0.5
+    assert anguelova.calc_V(x, params) == 1.9166666666666667
     assert np.allclose(
-        anguelova.calc_H(x, params), np.array([[1.0, 0.0], [0.0, 13.6449586]])
+        anguelova.calc_H(x, params), np.array([[ 0.10368764, -0.1483731 ], [-0.1483731, 0.03007954]])
     )
 
-    extent = [-1.0, 1.0, -1.0, 1.0]
+    extent = [0.0, 2.5, 0.0, np.pi]
     consistency_condition, epsilon_V, epsilon_H, eta_H, delta, omega = (
         anguelova.complete_analysis(params, *extent)
     )
+
+    assert np.nanmax(consistency_condition) <= 1
