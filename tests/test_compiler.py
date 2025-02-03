e4d9203178f7a552cee5a2008d89bdc1b8a5812b
Raúl <56522208+smups@users.noreply.github.com>
2025-02-03 10:33:16 +0100

    Revert "Revert "Merge changes for v0.10.0" (#54)" (#56)
    This reverts commit c5106eb2a67e6d41ec07de935e8836cc9268aef5.

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -22,55 +22,61 @@ import sympy
 from inflatox.compiler import *
 
 
-def test_cinflatox_printer():
-    x, y, a, b = sympy.symbols("x y a b")
-    printer = CInflatoxPrinter([x, y])
+@pytest.fixture
+def cprinter():
+    x, y, a, b, xdot, ydot = sympy.symbols("x y a b \\dot{{x}} \\dot{{y}}")
+    return CInflatoxPrinter([x, y], [xdot, ydot])
+
+
+@pytest.fixture
+def gslprinter():
+    x, y, a, b, xdot, ydot = sympy.symbols("x y a b \\dot{{x}} \\dot{{y}}")
+    return GSLInflatoxPrinter([x, y], [xdot, ydot])
+
+
+def test_cinflatox_printer(cprinter):
+    x, y, a, b, xdot, ydot = sympy.symbols("x y a b \\dot{{x}} \\dot{{y}}")
     # test basic symbols
-    assert "x[0]" == printer._print_Symbol(x)
-    assert "x[1]" == printer._print_Symbol(y)
-    assert "args[0]" == printer._print_Symbol(a)
-    assert "args[1]" == printer._print_Symbol(b)
+    assert "x[0]" == cprinter._print_Symbol(x)
+    assert "x[1]" == cprinter._print_Symbol(y)
+    assert "args[0]" == cprinter._print_Symbol(a)
+    assert "args[1]" == cprinter._print_Symbol(b)
+    assert "xdot[0]" == cprinter._print_Symbol(xdot)
+    assert "xdot[1]" == cprinter._print_Symbol(ydot)
 
     # test basic arithmetic
-    assert "pow(x[0], 2) + x[1]" == printer.doprint(x**2 + y)
-    assert "x[0]*x[1]" == printer.doprint(x * y)
+    assert "pow(x[0], 2) + x[1]" == cprinter.doprint(x**2 + y)
+    assert "x[0]*x[1]" == cprinter.doprint(x * y)
 
     # test some math.h funcs
-    assert "sqrt(args[0])*x[1]" == printer.doprint(sympy.sqrt(a) * y)
-    assert "sin(x[0])" == printer.doprint(sympy.sin(x))
+    assert "sqrt(args[0])*x[1]" == cprinter.doprint(sympy.sqrt(a) * y)
+    assert "sin(x[0])" == cprinter.doprint(sympy.sin(x))
 
 
-def test_gslinflatox_printer_headers():
+def test_gslinflatox_printer_headers(gslprinter):
     x, y = sympy.symbols("x y")
-    printer = GSLInflatoxPrinter([x, y])
-    printer.doprint(sympy.besselj(1, x))
-    assert printer.BESSELH in printer.required_headers
-    printer.doprint(sympy.hyper([], [1], x))
-    assert printer.HYPERH in printer.required_headers
+    gslprinter.doprint(sympy.besselj(1, x))
+    assert gslprinter.BESSELH in gslprinter.required_headers
+    gslprinter.doprint(sympy.hyper([], [1], x))
+    assert gslprinter.HYPERH in gslprinter.required_headers
 
 
-def test_gslinflatox_bessel():
+def test_gslinflatox_bessel(gslprinter):
     x, y, n = sympy.symbols("x y n")
-    printer = GSLInflatoxPrinter([x, y])
-    assert "gsl_sf_bessel_J0(x[0])" == printer.doprint(sympy.besselj(0, x))
-    assert "gsl_sf_bessel_J1(x[0])" == printer.doprint(sympy.besselj(1, x))
-    assert "gsl_sf_bessel_Jn(10, x[0])" == printer.doprint(sympy.besselj(10, x))
-    assert "gsl_sf_bessel_Jnu(0.50000000000000000, x[0])" == printer.doprint(
+    assert "gsl_sf_bessel_J0(x[0])" == gslprinter.doprint(sympy.besselj(0, x))
+    assert "gsl_sf_bessel_J1(x[0])" == gslprinter.doprint(sympy.besselj(1, x))
+    assert "gsl_sf_bessel_Jn(10, x[0])" == gslprinter.doprint(sympy.besselj(10, x))
+    assert "gsl_sf_bessel_Jnu(0.50000000000000000, x[0])" == gslprinter.doprint(
         sympy.besselj(0.5, x)
     )
 
 
-def test_gslinflatox_hyper():
-    x, y = sympy.symbols("x y")
-    printer = GSLInflatoxPrinter([x, y])
-    assert "gsl_sf_hyperg_2F0(0, 1, x[0])" == printer.doprint(
-        sympy.hyper([0, 1], [], x)
-    )
-    assert "gsl_sf_hyperg_2F1(0, 1, 2, x[0])" == printer.doprint(
-        sympy.hyper([0, 1], [2], x)
-    )
-    assert "gsl_sf_hyperg_1F1(0, 1, x[0])" == printer.doprint(sympy.hyper([0], [1], x))
-    assert "gsl_sf_hyperg_0F1(0, x[0])" == printer.doprint(sympy.hyper([], [0], x))
+def test_gslinflatox_hyper(gslprinter):
+    x, y, n = sympy.symbols("x y n")
+    assert "gsl_sf_hyperg_2F0(0, 1, x[0])" == gslprinter.doprint(sympy.hyper([0, 1], [], x))
+    assert "gsl_sf_hyperg_2F1(0, 1, 2, x[0])" == gslprinter.doprint(sympy.hyper([0, 1], [2], x))
+    assert "gsl_sf_hyperg_1F1(0, 1, x[0])" == gslprinter.doprint(sympy.hyper([0], [1], x))
+    assert "gsl_sf_hyperg_0F1(0, x[0])" == gslprinter.doprint(sympy.hyper([], [0], x))
     with pytest.raises(Exception) as exinfo:
-        printer.doprint(sympy.hyper([0, 3, 4], [1, 2], x))
+        gslprinter.doprint(sympy.hyper([0, 3, 4], [1, 2], x))
     assert "Cannot compute" in str(exinfo.value)
